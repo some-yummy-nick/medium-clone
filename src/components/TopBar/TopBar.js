8366fce1d5884dad55a6d588de71bbbbe9612a8f
Marat <yeffasol@mail.ru>
2020-07-01 19:02:35 +0300

    Добавляет имя пользователя в верхнее меню

--- src/components/TopBar/TopBar.js
+++ src/components/TopBar/TopBar.js
@@ -1,8 +1,11 @@
-import React from 'react'
+import React, {useContext} from 'react'
 import {Link, NavLink} from 'react-router-dom'
+import {CurrentUserContext} from '../../contexts/currentUser'
 
-export const TopBar = () => (
-  <>
+export const TopBar = () => {
+  const [currentUserState] = useContext(CurrentUserContext)
+
+  return (
     <nav className="navbar navbar-light">
       <div className="container">
         <Link to="/" className="navbar-brand">
@@ -14,20 +17,52 @@ export const TopBar = () => (
               Home
             </NavLink>
           </li>
-          <li className="nav-item">
-            <NavLink to="/login" className="nav-link">
-              Sign in
-            </NavLink>
-          </li>
-          <li className="nav-item">
-            <NavLink to="/register" className="nav-link">
-              Sign up
-            </NavLink>
-          </li>
+          {currentUserState.isLoggedIn === false && (
+            <>
+              <li className="nav-item">
+                <NavLink to="/login" className="nav-link">
+                  Sign in
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink to="/register" className="nav-link">
+                  Sign up
+                </NavLink>
+              </li>
+            </>
+          )}
+          {currentUserState.isLoggedIn && (
+            <>
+              <li className="nav-item">
+                <NavLink to="/articles/new" className="nav-link">
+                  <i className="ion-compose" />
+                  &nbsp; New post
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink
+                  to={`/profiles/${currentUserState.currentUser.username}`}
+                  className="nav-link"
+                >
+                  {currentUserState.currentUser.image && (
+                    <>
+                      <img
+                        src={currentUserState.currentUser.image}
+                        alt=""
+                        className="user-pic"
+                      />
+                      &nbsp;
+                    </>
+                  )}
+                  {currentUserState.currentUser.username}
+                </NavLink>
+              </li>
+            </>
+          )}
         </ul>
       </div>
     </nav>
-  </>
-)
+  )
+}
 
 export default TopBar
