11a6feb6b1b8154ae68156b6fce385172009cdf9
Marat <yeffasol@mail.ru>
2020-07-01 20:00:17 +0300

    Добавляет статьи на главную страницу

--- src/pages/GlobalFeed/GlobalFeed.js
+++ src/pages/GlobalFeed/GlobalFeed.js
@@ -1,5 +1,33 @@
-import React from 'react'
+import React, {useEffect} from 'react'
+import useFetch from '../../hooks/useFetch'
+import Feed from '../../components/Feed/Feed'
 
-export const GlobalFeed = () => <div className="container">GlobalFeed</div>
+export const GlobalFeed = () => {
+  const url = 'articles?limit=10&offset=0'
+  const [{isLoading, response, errors}, doFetch] = useFetch(url)
+  useEffect(() => {
+    doFetch()
+  }, [doFetch])
+  return (
+    <div className="home-page">
+      <div className="banner">
+        <div className="container">
+          <h1>Medium</h1>
+          <p>A place to share knowledge</p>
+        </div>
+      </div>
+      <div className="container page">
+        <div className="row">
+          <div className="col-md-9">
+            {isLoading && <div>Loading...</div>}
+            {errors && <div>Some error happened</div>}
+            {!isLoading && response && <Feed articles={response.articles} />}
+          </div>
+          <div className="col-md-3">Popular tags</div>
+        </div>
+      </div>
+    </div>
+  )
+}
 
 export default GlobalFeed
